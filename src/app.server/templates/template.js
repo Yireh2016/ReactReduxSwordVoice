103623cca89a798c432d66faf37c76200fef1e28
yireh2016 <jainer.calvetti@gmail.com>
2019-09-13 00:21:12 -0400

    adsense

--- src/app.server/templates/template.js
+++ src/app.server/templates/template.js
@@ -78,6 +78,16 @@ export default ({
 
           gtag('config', '${seoID}');
         </script>
+        <!-- Global Adsense -->
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3811803158014166",
+            enable_page_level_ads: true
+          });
+        </script>
+
+
 
       </head>
       
